1ed95c1364b5303c706044d54319a8371ff89e35
Noor_Rajab <rajabnoor115@gmail.com>
2025-09-09 19:42:06 +0300

    Done basic mathematics operations on the two integers

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,8 @@
+number1=10
+number2=5
+results_1=number1+number2
+results_2=number1-number2
+results_3=number1*number2
+print(f"Addition of {number1} and {number2} is {results_1}")
+print(f"Subtraction of {number1} and {number2} is {results_2}")
+print(f"Multiplication of {number1} and {number2} is {results_3}")
