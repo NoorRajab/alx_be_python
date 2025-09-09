d9a435fa114aa5716d28a2fe6df8462ad014f23a
Noor_Rajab <rajabnoor115@gmail.com>
2025-09-09 20:33:18 +0300

    Add hours_to_seconds.py and future_age_calculator.py

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,4 @@
+current_age = input("How old are you?")
+age_increase_in_2025 = 2050 - 2023
+future_age = int(current_age) + age_increase_in_2025
+print(f"In 2025, you will be {future_age} years old.") 
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,4 @@
+hours = 2
+seconds_in_hour = 3600
+seconds = seconds_in_hour * hours
+print(f"{hours} hour(s) is {seconds} seconds.")
